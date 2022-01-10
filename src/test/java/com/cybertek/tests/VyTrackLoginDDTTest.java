d90361c0bb5f61b6a8806534238b78bffb8ad419
14044 <parlakadalet@gmail.com>
2022-01-10 13:26:35 -0800

    Share Project

--- src/test/java/com/cybertek/tests/VyTrackLoginDDTTest.java
+++ src/test/java/com/cybertek/tests/VyTrackLoginDDTTest.java
@@ -1,5 +1,7 @@
 package com.cybertek.tests;
 
+import com.cybertek.utilities.ConfigurationReader;
+import com.cybertek.utilities.Driver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -9,6 +11,7 @@ public class VyTrackLoginDDTTest {
     public void setUp() {
         //set up browser etc if needed
         //open the url
+        Driver.getDriver().get(ConfigurationReader.getProperty("vytrack.url"));
     }
 
     @After
@@ -18,6 +21,12 @@ public class VyTrackLoginDDTTest {
 
     @Test
     public void loginDDTTest() {
+
+        String userName="user1";
+        String password="UserUser123";
+        String firstName="John";
+        String lastName="Doe";
+
         //open excel file
         //add page object
         //loop and read credentials
